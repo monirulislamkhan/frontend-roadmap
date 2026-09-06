2bdbab6109207708b7e9d934de99dc297914221c
monirulislamkhan <mr.monirulkhan@gmail.com>
2026-09-06 22:46:00 +0530

    Working on Javascript Async

--- console.js
+++ console.js
@@ -1,4 +1,4 @@
-const objList = [
+/* const objList = [
   {
     id: '01',
     name: 'John Doe',
@@ -12,4 +12,73 @@ const objList = [
 ];
 
 const mappedList = objList.filter((item) => item && item.id);
-console.log(mappedList);
+console.log(mappedList); */
+
+/* console.log('1');
+setTimeout(function () {
+  console.log('2');
+}, 300);
+
+setTimeout(function () {
+  console.log('3');
+}, 0);
+console.log('4'); */
+
+/* console.log('start');
+
+setTimeout(function () {
+  console.log('timeout');
+}, 0);
+
+Promise.resolve().then(function () {
+  console.log('promise');
+});
+
+console.log('end'); */
+
+/* const p = new Promise(function (resolve) {
+  setTimeout(function () {
+    resolve('Noida');
+  }, 1000);
+});
+
+console.log(p);
+
+p.then(function (value) {
+  console.log(value);
+}); */
+
+/* const p = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    reject('Server down');
+  }, 1000);
+});
+
+p.then(function (value) {
+  console.log('then:', value);
+})
+  .catch(function (e) {
+    console.log('catch:', e);
+  })
+  .finally(function () {
+    console.log('finally');
+  }); */
+
+function wait(ms) {
+  const p = new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve('Resolve ho gaya');
+    }, ms);
+  });
+  return p;
+}
+
+wait(1000).then(function (value) {
+  console.log(value);
+});
+
+function makeCity() {
+  const c = 'Noida';
+  return c;
+}
+console.log(makeCity());
