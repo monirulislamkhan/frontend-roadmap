76d97d32dfcaea8395b62209657190e95bd37e3b
monirulislamkhan <mr.monirulkhan@gmail.com>
2026-09-23 20:12:29 +0530

    some changes

--- month-3/data.js
+++ month-3/data.js
@@ -1,2 +1,7 @@
-const cities = ['New Delhi', 'Gurgaon', 'Kolkata'];
+console.log('data.js chal gayi');
 export { cities as cityList };
+const cities = ['New Delhi', 'Gurgaon', 'Kolkata'];
+
+const siteName = 'Hco Real Estates';
+const siteName1 = 'Others name';
+export default siteName;
--- month-3/main.js
+++ month-3/main.js
@@ -1,3 +1,6 @@
+console.log('main shuru');
 import { cityList } from './data.js';
+import brandName from './data.js';
 
 console.log(cityList);
+console.log(brandName);
